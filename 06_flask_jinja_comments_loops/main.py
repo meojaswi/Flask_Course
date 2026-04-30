e136c7d14c58466434308eae08ac653b1367ec39
Ojaswi ‎ <ojaswi3236@gmail.com>
2026-05-01 00:51:25 +0530

    Add Jinja comments and loops

---
+++ 06_flask_jinja_comments_loops/main.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    marks = {
+        "John":59,
+        "Saurabh":71,
+        "Mark":74,
+        "Jeff":85,
+        "Alexa":95,
+        "Lily":100
+    }
+    return render_template("index.html",marks= marks)
+
+app.run(debug=True)
+
+
